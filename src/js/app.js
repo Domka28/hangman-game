bf8730536fd6d159edef395b6b85bb32e40290d7
Domka28 <119631616+Domka28@users.noreply.github.com>
2023-12-11 21:36:35 +0100

    Fix bug with const assigment

--- src/js/app.js
+++ src/js/app.js
@@ -11,7 +11,7 @@ const showBtn = document.querySelector(".show-sentence")
 
 const arrayOfLetters = [];
 let counter = 1;
-const currentSentence = ''
+let currentSentence = ''
 const alphabet = ["A", "Ą", "B", "C", "Ć", "D", "E", "Ę", "F", "G", "H", "I", "J", "K", "L", "Ł", "M", "N", "Ń", "O", "Ó", "P", "Q", "R", "S", "Ś", "T", "U", "V", "W", "X", "Y", "Z", "Ż", "Ź"]
 
 
